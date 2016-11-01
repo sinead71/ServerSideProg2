c16e0ffc50aabbea7f7ad57f224e45b9b52ed8a5
sinead.71 <cooneysinead71.com>
2016-11-01 20:50:45 +0000

    adding html to the header of the index page

--- assignment/header.php
+++ assignment/header.php
@@ -1,6 +1,9 @@
 <?php 
     echo ('<div id="header">');
         echo ('<h1 id="pagetitle">Products</h1>');
+        echo ('<form method="POST" action="cart.php">');
+            echo ('<button>Shopping Cart</button>');
+        echo ('</form>');
     echo ('</div>');
     
     
---
+++ assignment/index.php
@@ -0,0 +1,12 @@
+<?php 
+    session_start();
+    include("header.php");
+    include("pageContent.php");
+    include("footer.php");
+?>
+        
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css"/>        
+    </head>   
+</html>
