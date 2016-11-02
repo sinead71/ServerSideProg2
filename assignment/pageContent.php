bd21d4e19d18490ec9024e900920482f122baeea
sinead.71 <cooneysinead71.com>
2016-11-02 16:36:28 +0000

    css added to the products page

--- assignment/pageContent.php
+++ assignment/pageContent.php
@@ -1,4 +1,9 @@
 <?php 
-//add some products to this section! :)
-
+    echo ("<div class='products'>");
+        echo ("<h2>product One</h2>");
+        echo ("<img src='https://codekeyboards.com/galleria/9.jpg' alt='a keyboard'></a>");
+        echo ("<form method='POST' action='cart.php'>");
+            echo ("<button id='addBtn'>Add To Cart</button>");
+        echo ("</form>");
+    echo ("</div>");
 ?>
