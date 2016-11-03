1f5af045019ab5f7d1446046c0d2c110fb014294
sinead.71 <cooneysinead71.com>
2016-11-03 19:34:33 +0000

    adding a log out button

--- assignment/header.php
+++ assignment/header.php
@@ -4,6 +4,9 @@
         echo ('<form method="POST" action="cart.php">');
             echo ('<button id="cartBtn">Shopping Cart</button>');
         echo ('</form>');
+        echo ('<form method="POST" action="logIn.php">');
+            echo ('<button id="logOutBtn">Log Out</button>');
+        echo ('</form>');
     echo ('</div>');
     
   
