9cb2439e8c3741f72fc820479e91f2a0112605a1
sinead.71 <cooneysinead71.com>
2016-11-05 16:36:55 +0000

    linking the products on the database to be seen on the screen

--- assignment/pageContent.php
+++ assignment/pageContent.php
@@ -1,6 +1,8 @@
 <?php 
+include("products.php");
+
     echo ("<div class='products'>");
-        echo ("<h2>Keyboard</h2>");
+        //echo ("<h2>Keyboard</h2>");
         echo ("<img src='https://codekeyboards.com/galleria/9.jpg' alt='a keyboard'></a>");
         echo ("<form method='POST' action='cart.php'>");
             echo ("<button id='addBtn'>Add To Cart</button>");
--- assignment/products.php
+++ assignment/products.php
@@ -1,13 +1,26 @@
 <?php 
-    session_start();
+    //session_start();
     include("header.php");
     include("pageContent.php");
     include("footer.php");
     include ("database.php");
     
-    if(isset($_POST("cart"))){
-           
-    }
+    $stmt = $conn->prepare("SELECT * FROM products");
+    $stmt->execute();
+    
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+        echo("<div class='products'");
+            echo("<h2>". $row['title'] ."</h2>");
+        
+        /*$title = $row['title'];
+        $prod_id = $row['prod_id'];
+        $desc = $row['description'];
+        $image_url = $row['image_url'];
+        $price = $row['price'];
+        */
+        
+    }          
     
 ?>
         
