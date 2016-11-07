2d441f7a5bfeac61c142dddb8d7b8306e8e9dcb3
sinead.71 <cooneysinead71.com>
2016-11-07 18:27:35 +0000

    adding delete button

--- assignment/cart.php
+++ assignment/cart.php
@@ -1,6 +1,9 @@
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<body class="cart"></body>
 <?php 
 session_start();
 include("database.php");
+
 
 /*loop through session array
 where the id in the array matches the id in the database 
@@ -16,11 +19,23 @@ foreach ($_SESSION['shoppingCart'] as $prodId){
             echo("<h2>". $row['title']." &euro;" .$row['price']  ."</h2>");
             echo("<p>". $row['description'] ."</p>");
             echo ("<img src='". $row['image_url'] ."' alt='a keyboard'></a>");           
+            echo("<form method='POST' id='delete'");
+                echo("<input type='hidden' value='".$row['prod_id']."' name='cartProdId'>");
+                echo("<button id='deleteProd' name='deleteProd'>Delete</button>");
+            echo("</form>");
         echo("</div>");
     }
 }
 
 
+if(isset($_POST['delete'])){
+    $deletingProduct = $_POST['cartProdId'];
+    foreach($_SESSION['shoppingCart'] as $prodId => $val){
+        if($val == $deletingProduct){
+            unset($_SESSION['shoppingCart'][''$prodId]);
+        }   
+    }
+}
 
 
 ?>
