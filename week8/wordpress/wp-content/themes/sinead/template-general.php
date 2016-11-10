6aca5d4afeb5f1e78e9460b0920924c12f20b8af
sinead.71 <cooneysinead71.com>
2016-11-10 14:07:37 +0000

    class code examples

--- week8/wordpress/wp-content/themes/sinead/template-general.php
+++ week8/wordpress/wp-content/themes/sinead/template-general.php
@@ -29,7 +29,7 @@ $vists = get_option('page_views');
 //update_option("page_views");
 
 
-
+/*
 update_user_meta(3, 'last_name', 'ryan' );
 $userId = 
 get_current_user_id();
@@ -39,5 +39,24 @@ echo get_current_user_id();
 echo ("<br>");
 $allData = get_user_meta(2);
 echo($allData);
+echo ("<br>");
+*/
+echo ("<br>");
+global $wpdb;
+
+$user_count = $wpdb->get_var(
+    "SELECT user_email FROM wp_users WHERE ID=1"
+);
+
+echo $user_email;
+echo ("<br>");
+
+$user_data = $wpdb->get_row(
+    "SELECT * FROM wp_users WHERE ID=1"
+);
+
+//can replace user_email with any other wp_user info from db
+echo $user_data->user_email;
+
 
 ?>
